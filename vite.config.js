026fb9c3ca21092181d0aba40a145cafc9fc12f1
bebebebeb1 <n.zaharii@student.neoversity.com.ua>
2026-02-13 18:10:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,41 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig({
-  root: 'src',                 // index.html в src
-  base: '/kyrcova3.github.io/',
-
-  build: {
-    outDir: '../dist',         // dist у корені проєкту
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) return 'vendor';
+          },
+          entryFileNames: '[name]-[hash].js',
+          chunkFileNames: '[name]-[hash].js',
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
 
-  plugins: [
-    injectHTML(),
-    FullReload(['./**/*.html']),
-    SortCss({ sort: 'mobile-first' }),
-  ],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({ sort: 'mobile-first' }),
+    ],
+  };
 });
