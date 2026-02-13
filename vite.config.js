0a8300406826ef1bca7c566d49248b12d917a063
bebebebeb1 <n.zaharii@student.neoversity.com.ua>
2026-02-13 18:42:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/kyrcova3.github.io/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
